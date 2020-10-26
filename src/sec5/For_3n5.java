7313fbd72a523495f139c363e0bd9c9127a54e1c
Ng Kai Meng <kmng@u.nus.edu>
2020-10-26 18:30:55 +0800

    Sec 5

---
+++ src/sec5/For_3n5.java
@@ -0,0 +1,25 @@
+package sec5; //com.timbuchalka;
+
+public class For_3n5 {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        int sum = 0;
+
+        for(int i=1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                count++;
+                sum += i;
+                System.out.println("Found number = " + i);
+            }
+
+            if(count == 5) {
+                break;
+            }
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
